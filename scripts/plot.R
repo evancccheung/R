606ec3d9aa1168492916486fd462c29c8df2aa53
Evan Cheung <evancc@connect.hku.hk>
2023-08-03 13:05:48 +0200

    updated scripts 13:05

--- scripts/plot.R
+++ scripts/plot.R
@@ -30,4 +30,36 @@ ggplot(data=yearly_sex_counts) +
   geom_line() + 
   facet_grid(rows = vars(genus))
 
-ggsave("fig_output/my_plot.png")
+ggsave("fig_output/my_plot.png",
+       height = 10,
+       width = 20,
+       units = "cm",
+       dpi = 200)
+
+# Radar/Spider Chart
+require(fmsb)
+
+data <- as.data.frame(matrix(sample(2:20, 10, replace=T), ncol=10))
+colnames(data) <- c("math", "english", "biology", "music", "R-coding",
+                    "data-viz", "french", "physics", "statistics", "sport")
+head(data)
+
+set.seed(1234)
+data <- rbind(rep(20, 10), rep(0, 10), data)
+
+radarchart(data)
+
+# Boxplot
+surveys_complete %>% 
+  ggplot() + 
+  aes(x = plot_type,
+      y = weight) + 
+  geom_boxplot()
+
+# Violin Plot
+surveys_complete %>% 
+  ggplot() + 
+  aes(x = plot_type,
+      y = weight) + 
+  geom_violin() + 
+  geom_jitter(alpha = 0.2)
